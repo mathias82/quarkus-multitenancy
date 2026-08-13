32a90c1bf943f7060ff0e4fc3def37006c8b5c51
Matthaios Stavrou <4541512+mathias82@users.noreply.github.com>
2026-08-13 20:12:20 +0300

    demo: expose path resolution scenario

--- quarkus-multitenancy-demo/src/main/java/io/github/demo/resource/UserResource.java
+++ quarkus-multitenancy-demo/src/main/java/io/github/demo/resource/UserResource.java
@@ -20,7 +20,12 @@ import java.util.Optional;
 
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 import io.github.demo.entity.User;
@@ -40,9 +45,6 @@ public class UserResource {
     }
 
     @POST
-    @Path("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     public User create(User user) {
         user.persist();
@@ -52,6 +54,12 @@ public class UserResource {
     @GET
     @Path("/tenant")
     public Optional<String> tenant() {
+        return tenantContext.getTenantId();
+    }
+
+    @GET
+    @Path("/tenant/path/{tenant}")
+    public Optional<String> pathTenant(@PathParam("tenant") String ignored) {
         return tenantContext.getTenantId();
     }
 }
